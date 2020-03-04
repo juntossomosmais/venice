9213abd457af0efa6ba36878957ec37fb3e12bde
felipefialho <hi@felipefialho.com>
2020-03-04 19:45:12 -0300

    feat: add addon-knobs

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,14 @@
 import { configure, addDecorator } from '@storybook/html'
 import { withTests } from '@storybook/addon-jest'
+import { withKnobs } from '@storybook/addon-knobs'
 
 addDecorator(
 	withTests({
 		filesExt: '.spec.ts',
 	})
 )
+
+addDecorator(withKnobs)
 
 const req = require.context('../src', true, /\.stories\.tsx$/)
 function loadStories() {
