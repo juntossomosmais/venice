99a4790492748eecb4f1fa20940841e3d732a633
felipefialho <hi@felipefialho.com>
2020-03-18 19:02:00 -0300

    feat: add test using css modules with react

--- packages/react-ds/src/components/Button/Button.stories.tsx
+++ packages/react-ds/src/components/Button/Button.stories.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
+import Button from './Button';
 
 export default {
   title: 'Button',
   component: Button,
 };
 
-export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
+export const Text = () => <Button />;
 
 export const Emoji = () => (
-  <Button onClick={action('clicked')}>
-    <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
-    </span>
-  </Button>
+  <Button />
 );
