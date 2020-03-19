8665f1455b8f3b2c86774a07da4eacf940cfb19e
felipefialho <hi@felipefialho.com>
2020-03-19 19:34:19 -0300

    chore: change folder structure

--- packages/vue-ds/.storybook/main.js
+++ packages/vue-ds/.storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../../src/**/*.stories.(js|jsx|ts|tsx|mdx)'],
+  stories: ['../src/**/*.stories.(js|jsx|ts|tsx|mdx)'],
   addons: [
     '@storybook/addon-actions',
     {
