98565e3045173294121814f56d79323df5890961
felipefialho <hi@felipefialho.com>
2020-03-19 19:29:42 -0300

    chore: add initial structure to storybook on vue

--- packages/vue-ds/.storybook/config.ts
+++
@@ -1,3 +0,0 @@
-import { configure } from '@storybook/vue';
-
-configure(require.context('../src', true, /\.stories\.tsx?$/), module)
--- packages/vue-ds/.storybook/webpack.config.ts
+++
@@ -1,7 +0,0 @@
-import { TypeScriptLoader } from '@venice/core/storybook'
-
-module.exports = ({ config }) => {
-	config.module.rules.push(TypeScriptLoader);
-	config.resolve.extensions.push('.ts', '.tsx');
-	return config;
-}
