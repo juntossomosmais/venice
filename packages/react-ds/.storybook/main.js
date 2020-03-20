cb1d5d094082d849b02fe64dbf7639c39513db27
felipefialho <hi@felipefialho.com>
2020-03-20 18:17:20 -0300

    chore: add simple config to storybook

---
+++ packages/react-ds/.storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ['../src/**/*.stories.(js|jsx|ts|tsx|mdx)'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+  ],
+};
