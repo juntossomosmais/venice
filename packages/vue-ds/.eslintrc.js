1ceaae69d335b88c686799d3b175a9b770318174
felipefialho <hi@felipefialho.com>
2020-03-20 17:39:39 -0300

    chore: add eslint to vue

--- packages/vue-ds/.eslintrc.js
+++ packages/vue-ds/.eslintrc.js
@@ -1,3 +1,29 @@
 module.exports = {
-	extends: ['../../.eslintrc.js', 'plugin:vue/recommended'],
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    '../../.eslintrc.js',
+    'plugin:vue/essential',
+    '@vue/typescript/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }
