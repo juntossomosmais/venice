ac1078bb2a9f4c1fef49268c1ae60ff2c5c1865e
felipefialho <hi@felipefialho.com>
2020-03-20 12:57:40 -0300

    chore: add lint import

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,14 +11,48 @@ module.exports = {
 		ecmaVersion: 2019,
 		sourceType: 'module',
 	},
+	settings: {
+		'import/resolver': {
+			node: {
+				extensions: ['.js', '.jsx', '.vue', 'ts', 'tsx', 'mdx'],
+			},
+		},
+	},
 	extends: [
 		'eslint:recommended',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:import/typescript',
 		'plugin:@typescript-eslint/recommended',
 		'prettier',
 	],
-	plugins: ['prettier', 'jest', '@typescript-eslint'],
+	plugins: ['prettier', 'jest', 'import', '@typescript-eslint'],
 	rules: {
 		'prettier/prettier': ['error'],
 		'@typescript-eslint/explicit-function-return-type': 'off',
+		'import/no-unresolved': 'off',
+		'import/order': [
+			'error',
+			{
+				groups: [
+					'builtin',
+					'external',
+					'internal',
+					'parent',
+					'sibling',
+					'index',
+				],
+				'newlines-between': 'always',
+				alphabetize: { order: 'asc', caseInsensitive: true },
+				pathGroups: [
+					{
+						pattern: 'react',
+						group: 'external',
+						position: 'before',
+					},
+				],
+				pathGroupsExcludedImportTypes: ['react'],
+			},
+		],
 	},
 }
