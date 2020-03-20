c762b70c1f81e15bb0a42eda2ca348ed51a8a2e7
felipefialho <hi@felipefialho.com>
2020-03-20 12:58:10 -0300

    chore: lint file

--- packages/vue-ds/tests/unit/example.spec.ts
+++ packages/vue-ds/tests/unit/example.spec.ts
@@ -1,5 +1,5 @@
-import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount } from '@vue/test-utils'
 
 describe('HelloWorld.vue', () => {
 	it('renders props.msg when passed', () => {
