9af0a2ca78eb8baf7b1c0b416799a92fdbb14130
felipefialho <hi@felipefialho.com>
2020-03-24 18:17:40 -0300

    feat: some changes to use css modules

--- packages/react-ds/src/components/Button/Button.tsx
+++ packages/react-ds/src/components/Button/Button.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import { IButton } from '@venice/core/models'
 import styles from '@venice/styles/src/components/Button.module.scss'
 
-const Button = () => {
+const Button = (props: IButton) => {
+  const { children, ...rest } = props
+
+  const [color] = useState(props.color)
+  const [variant] = useState(props.variant)
+
+  const setStyle = (c: string | undefined) => (c ? `${styles[c]}` : '')
+
   return (
     <>
       <button
-        className={styles.btn}
-        onClick={() => alert('I am styled with CSS Modules')}
+        className={`${styles.btn} ${setStyle(color)} ${setStyle(variant)}`}
+        {...rest}
       >
-        I am button 2 - Press Me
+        {children}
       </button>
     </>
   )
