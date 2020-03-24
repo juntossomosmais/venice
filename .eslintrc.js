bcba849db04de79049f5d40bbe3123db02ddd58a
felipefialho <hi@felipefialho.com>
2020-03-24 18:17:14 -0300

    chore: change some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,62 +1,64 @@
 module.exports = {
-	extends: [
-		'eslint:recommended',
-		'plugin:import/errors',
-		'plugin:import/warnings',
-		'plugin:import/typescript',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:mdx/recommended',
-		'prettier',
-	],
-	plugins: ['prettier', 'jest', 'import', '@typescript-eslint'],
-	env: {
-		browser: true,
-		node: true,
-		es6: true,
-		jest: true,
-		jasmine: true,
-	},
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaVersion: 2019,
-		sourceType: 'module',
-		ecmaFeatures: {
-			jsx: true,
-		},
-	},
-	settings: {
-		'import/resolver': {
-			node: {
-				extensions: ['.js', '.jsx', '.vue', 'ts', 'tsx', 'mdx'],
-			},
-		},
-	},
-	rules: {
-		'prettier/prettier': ['error'],
-		'@typescript-eslint/explicit-function-return-type': 'off',
-		'import/no-unresolved': 'off',
-		'import/order': [
-			'error',
-			{
-				groups: [
-					'builtin',
-					'external',
-					'internal',
-					'parent',
-					'sibling',
-					'index',
-				],
-				'newlines-between': 'always',
-				alphabetize: { order: 'asc', caseInsensitive: true },
-				pathGroups: [
-					{
-						pattern: 'react',
-						group: 'external',
-						position: 'before',
-					},
-				],
-				pathGroupsExcludedImportTypes: ['react'],
-			},
-		],
-	},
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:mdx/recommended',
+    'prettier',
+  ],
+  plugins: ['prettier', 'jest', 'import', '@typescript-eslint'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+    jasmine: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.vue', 'ts', 'tsx', 'mdx'],
+      },
+    },
+  },
+  rules: {
+    'prettier/prettier': ['error'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/interface-name-prefix': 'always',
+    'import/no-unresolved': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+      },
+    ],
+  },
 }
