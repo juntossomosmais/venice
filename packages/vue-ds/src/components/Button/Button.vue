55f4467849e40f955f2a42ab28635693c102b3ba
felipefialho <hi@felipefialho.com>
2020-03-25 17:48:10 -0300

    feat: add button to vue

--- packages/vue-ds/src/components/Button/Button.vue
+++ packages/vue-ds/src/components/Button/Button.vue
@@ -1,13 +1,26 @@
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+import { IButton } from '@venice/core/models'
 
 @Component
-export default class MyButton extends Vue {}
+export default class Button extends Vue {
+  @Prop({ default: 'default' }) color!: IButton['color']
+  @Prop({ default: 'filled' }) variant!: IButton['variant']
+  @Prop() href!: IButton['href']
+}
 </script>
 
 <template>
-  <button :class="$style.btn">
-    I am button 2 - Press Me
+  <a
+    v-if="href"
+    :class="[$style.btn, $style[color], $style[variant]]"
+    :href="href"
+  >
+    <slot />
+  </a>
+  <button v-else :class="[$style.btn, $style[color], $style[variant]]">
+    <slot />
   </button>
 </template>
 
