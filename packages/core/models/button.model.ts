c2f66b4032d79214a169dfe65c70a22f90ff71fb
felipefialho <hi@felipefialho.com>
2020-03-27 17:21:01 -0300

    feat: add as optional

--- packages/core/models/button.model.ts
+++ packages/core/models/button.model.ts
@@ -1,8 +1,8 @@
 export interface IButton {
   /**  'default' | 'primary' | 'secondary' */
-  color: 'default' | 'primary' | 'secondary'
+  color?: 'default' | 'primary' | 'secondary'
   /** 'fill' | 'outline' | 'text' */
-  variant: 'filled' | 'outlined' | 'text'
+  variant?: 'filled' | 'outlined' | 'text'
   /** true | 'false */
   disabled?: boolean
   /** href <a> */
