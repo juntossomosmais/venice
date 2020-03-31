98e6b3f385c0446cab3c7b0d5ee3ab5aac5de503
felipefialho <hi@felipefialho.com>
2020-03-31 19:41:21 -0300

    feat: add rollup to build lib

---
+++ packages/react-ds/rollup.config.js
@@ -0,0 +1,50 @@
+import autoprefixer from 'autoprefixer'
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import external from 'rollup-plugin-peer-deps-external'
+import postcss from 'rollup-plugin-postcss'
+import typescript from 'rollup-plugin-typescript2'
+
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.tsx',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    external(),
+    resolve({
+      browser: true,
+    }),
+    typescript(),
+    commonjs({
+      include: /node_modules/,
+      exclude: ['**/*.stories.js'],
+      namedExports: {
+        'node_modules/react/react.js': [
+          'Children',
+          'Component',
+          'PropTypes',
+          'createElement',
+        ],
+        'node_modules/react-dom/index.js': ['render'],
+      },
+    }),
+    postcss({
+      extract: false,
+      modules: true,
+      use: ['sass'],
+      plugins: [autoprefixer()],
+    }),
+  ],
+}
