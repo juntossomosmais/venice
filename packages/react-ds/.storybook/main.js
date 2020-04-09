ca6043abdeed97d90c1980b06c306bb4400261f5
felipefialho <hi@felipefialho.com>
2020-04-09 17:39:44 -0300

    feat: accept stories from core

--- packages/react-ds/.storybook/main.js
+++ packages/react-ds/.storybook/main.js
@@ -1,11 +1,14 @@
 module.exports = {
-  stories: ['../src/**/*.stories.(js|jsx|ts|tsx|mdx)'],
+  stories: [
+    '../../core/**/*.stories.(js|jsx|ts|tsx|mdx)',
+    '../src/**/*.stories.(js|jsx|ts|tsx|mdx)'
+  ],
   addons: [
     'storybook-addon-themes',
     '@storybook/addon-knobs/register',
     '@storybook/addon-a11y/register',
-    '@storybook/addon-notes/register',
     '@storybook/addon-viewport/register',
+    //'@storybook/addon-notes/register',
     '@storybook/preset-create-react-app',
     {
       name: '@storybook/addon-docs',
--- packages/vue-ds/.storybook/main.js
+++ packages/vue-ds/.storybook/main.js
@@ -1,27 +1,36 @@
-const addonDocsWithVue = {
-  name: '@storybook/addon-docs',
-  options: {
-    babelOptions: {
-      presets: [
-        [
-          '@vue/cli-plugin-babel/preset',
-          {
-            jsx: false,
-          },
-        ],
-      ],
-    },
-  },
-}
-
 module.exports = {
-  stories: ['../src/**/*.stories.(js|jsx|ts|tsx|mdx)'],
+  stories: [
+    '../../core/**/*.stories.(js|jsx|ts|tsx|mdx)',
+    '../src/**/*.stories.(js|jsx|ts|tsx|mdx)'
+  ],
   addons: [
     'storybook-addon-themes',
     '@storybook/addon-knobs/register',
     '@storybook/addon-a11y/register',
-    '@storybook/addon-notes/register',
+    //'@storybook/addon-notes/register',
     '@storybook/addon-viewport/register',
-    addonDocsWithVue,
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        babelOptions: {
+          presets: [
+            [
+              '@vue/cli-plugin-babel/preset',
+              {
+                jsx: false
+              }
+            ],
+          ],
+        },
+      },
+    }
   ],
+  webpackFinal: async config => {
+    config.module.rules = [{
+      test: /\.md$/,
+      use: ['babel-loader', '@mdx-js/loader'],
+    }, ...config.module.rules.filter(rule => rule.test.source !== '\\.md$')]
+
+    return config
+  },
 }
