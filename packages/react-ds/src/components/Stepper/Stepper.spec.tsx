0a017c1b111054e0c8309227a8192bcd8882145f
Elizeu Louzeiro <elizeu.louzeiro@avenuecode.com>
2020-04-24 18:59:12 -0300

    feat: created stepper tests

---
+++ packages/react-ds/src/components/Stepper/Stepper.spec.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { render } from '@testing-library/react'
+
+import '@testing-library/jest-dom'
+
+import { completed } from '@venice/core/tests/mocks/stepper-mocks'
+
+import Stepper from './Stepper'
+
+describe('<Stepper />', () => {
+  it('should create a stepbar with 5 steps', () => {
+    const { getByTestId } = render(<Stepper steps={completed} />)
+
+    expect(getByTestId('stepper').children).toHaveLength(5)
+  })
+})
