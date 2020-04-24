ed506e9953f1ba2337e84c823dbc949ed9a0ca2a
Elizeu Louzeiro <elizeu.louzeiro@avenuecode.com>
2020-04-24 18:54:25 -0300

    feat: created stepper mocks

---
+++ packages/core/tests/mocks/stepper-mocks.ts
@@ -0,0 +1,47 @@
+export const completed = [
+  {
+    title: 'Resgate realizado',
+    subtitle: '31/03/2020',
+    completed: true,
+  },
+  {
+    title: 'Pedido confirmado',
+    subtitle: '31/03/2020',
+    completed: true,
+  },
+  {
+    title: 'Na transportadora',
+    subtitle: '31/03/2020',
+    completed: true,
+  },
+  {
+    title: 'Resgate realizado',
+    subtitle: '31/03/2020',
+    completed: true,
+  },
+  {
+    title: 'Entregue',
+    subtitle: '31/03/2020',
+    completed: true,
+  },
+]
+
+export const started = [
+  {
+    title: 'Resgate realizado',
+    subtitle: '31/03/2020',
+    completed: true,
+  },
+  {
+    title: 'Pedido confirmado',
+  },
+  {
+    title: 'Na transportadora',
+  },
+  {
+    title: 'Resgate realizado',
+  },
+  {
+    title: 'Entregue',
+  },
+]
