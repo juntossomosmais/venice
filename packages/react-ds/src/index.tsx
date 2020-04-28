8d093565c8bcc0972e605acd537024ece1bf327a
Elizeu Louzeiro <elizeu.louzeiro@avenuecode.com>
2020-04-28 09:15:53 -0300

    feat: added stepper component

--- packages/react-ds/src/index.tsx
+++ packages/react-ds/src/index.tsx
@@ -1,1 +1,2 @@
 export { default as Button } from './components/Button/Button'
+export { default as Stepper } from './components/Stepper/Stepper'
--- packages/vue-ds/src/index.ts
+++ packages/vue-ds/src/index.ts
@@ -1,1 +1,2 @@
 export { default as MyButton } from './components/Button/Button.vue'
+export { default as Stepper } from './components/Stepper/Stepper.vue'
