d8e3f4664ec71fabad2ddf912c7d10b93d681532
Elizeu Louzeiro <elizeu.louzeiro@avenuecode.com>
2020-04-28 14:58:17 -0300

    feat: removed proptypes and defaultvalues properties

--- packages/react-ds/src/components/Stepper/Stepper.tsx
+++ packages/react-ds/src/components/Stepper/Stepper.tsx
@@ -1,11 +1,9 @@
 import React, { FC, useCallback } from 'react'
 
-import PropTypes from 'prop-types'
-
 import { IStepper } from '@venice/core/models'
 import styles from '@venice/styles/components/Stepper.module.scss'
 
-const Stepper: FC<IStepper> = ({ steps }: IStepper) => {
+const Stepper: FC<IStepper> = ({ steps = [] }: IStepper) => {
   const isActive = useCallback(
     (completed: boolean): string => (completed ? styles['active'] : ''),
     []
@@ -41,20 +39,6 @@ const Stepper: FC<IStepper> = ({ steps }: IStepper) => {
       ))}
     </ul>
   )
-}
-
-Stepper.propTypes = {
-  steps: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      subtitle: PropTypes.string,
-      completed: PropTypes.bool.isRequired,
-    }).isRequired
-  ).isRequired,
-}
-
-Stepper.defaultProps = {
-  steps: [],
 }
 
 export default Stepper
