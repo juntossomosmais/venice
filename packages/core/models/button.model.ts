94c6e8a9c282df6e09af9e6a129179b607a9785e
felipefialho <hi@felipefialho.com>
2020-04-28 18:00:52 -0300

    chore: put on alphabetical order

--- packages/core/models/button.model.ts
+++ packages/core/models/button.model.ts
@@ -1,12 +1,12 @@
 export interface IButton {
   /**  'default' | 'primary' | 'secondary' */
   color?: 'default' | 'primary' | 'secondary'
-  /** 'fill' | 'outline' | 'text' */
-  variant?: 'filled' | 'outlined' | 'text'
   /** true | 'false */
   disabled?: boolean
   /** href <a> */
   href?: string
   /** 'medium' | 'small' */
   size?: 'medium' | 'small'
+  /** 'fill' | 'outline' | 'text' */
+  variant?: 'filled' | 'outlined' | 'text'
 }
