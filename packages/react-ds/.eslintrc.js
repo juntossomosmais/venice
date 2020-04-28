6e6701289ee6dc955e3bba7a30f21bbce163f558
felipefialho <hi@felipefialho.com>
2020-04-28 18:41:56 -0300

    chore: remove props

--- packages/react-ds/.eslintrc.js
+++ packages/react-ds/.eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
-	extends: ['../../.eslintrc.js', 'plugin:react/recommended'],
+  extends: ['../../.eslintrc.js', 'plugin:react/recommended'],
+  rules: {
+    'react/prop-types': 'off',
+  },
 }
