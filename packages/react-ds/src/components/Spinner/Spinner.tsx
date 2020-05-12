3ef953ef4f16cd4db6cc233cc165b4588553f344
felipefialho <hi@felipefialho.com>
2020-05-12 11:50:59 -0300

    feat(spinner): add contrast colors

--- packages/react-ds/src/components/Spinner/Spinner.tsx
+++ packages/react-ds/src/components/Spinner/Spinner.tsx
@@ -3,14 +3,26 @@ import React from 'react'
 import { ISpinner } from '@venice/core/models'
 import styles from '@venice/styles/components/Spinner.module.scss'
 
-const Spinner = ({ color = 'default', size = 36, ...rest }: ISpinner) => {
+interface ISpinnerProps extends ISpinner {
+  /** string */
+  className: string
+}
+
+const Spinner = ({
+  className = '',
+  color = 'default',
+  size = 36,
+  ...rest
+}: ISpinnerProps) => {
   return (
     <div
       className={`
       ${styles.spinner}
       ${styles[color]}
+      ${className}
     `}
       style={{
+        fontSize: size,
         height: size,
         width: size,
       }}
