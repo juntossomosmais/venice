794668b1f06cc1d271df1ac64c1eaf47de3f36d3
felipefialho <hi@felipefialho.com>
2020-05-12 14:39:43 -0300

    feat(spinner): add contrast colors

--- packages/core/models/spinner.model.ts
+++ packages/core/models/spinner.model.ts
@@ -1,6 +1,13 @@
 export interface ISpinner {
   /**  'default' | 'primary' | 'secondary' */
-  color?: 'default' | 'primary' | 'secondary'
+  color?:
+    | 'default'
+    | 'primary'
+    | 'secondary'
+    | 'default-contrast'
+    | 'primary-contrast'
+    | 'secondary-contrast'
+    | string
   /** number */
   size?: number
 }
