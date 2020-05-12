e544a2f9a2b79daaa84bb0bedb973bcbcadd2fc8
felipefialho <hi@felipefialho.com>
2020-05-12 16:15:06 -0300

    feat(button): add loading state

--- packages/vue-ds/src/components/Button/Button.vue
+++ packages/vue-ds/src/components/Button/Button.vue
@@ -1,20 +1,27 @@
 <template>
   <component
     :is="href ? 'a' : 'button'"
-    :class="['btn', color, variant, size]"
+    :class="['btn', color, variant, size, isLoading ? 'isLoading' : '']"
     :href="href"
     :role="href ? '' : 'button'"
+    :aria-busy="isLoading"
     @click="$emit('click')"
   >
-    <span :class="'startIcon'">
+    <Spinner
+      v-if="isLoading"
+      :color="variant === 'filled' ? `${color}-contrast` : color"
+      :size="size === 'small' ? 16 : 24"
+      class="loading"
+    />
+    <span class="startIcon">
       <!-- @slot To add icon at the beginning -->
       <slot name="startIcon" />
     </span>
-    <span :class="'children'">
+    <span class="children">
       <!-- @slot To add something, like a text -->
       <slot />
     </span>
-    <span :class="'endIcon'">
+    <span class="endIcon">
       <!-- @slot To add icon at the end -->
       <slot name="endIcon" />
     </span>
@@ -26,11 +33,18 @@ import { Component, Vue, Prop } from 'vue-property-decorator'
 
 import { IButton } from '@venice/core/models'
 
-@Component
+import Spinner from '../Spinner/Spinner.vue'
+
+@Component({
+  components: {
+    Spinner,
+  },
+})
 export default class Button extends Vue {
   @Prop({ default: 'default' }) color!: IButton['color']
   @Prop({ default: 'filled' }) variant!: IButton['variant']
   @Prop({ default: 'medium' }) size!: IButton['size']
+  @Prop({ default: false }) isLoading!: IButton['isLoading']
   @Prop() href!: IButton['href']
 }
 </script>
