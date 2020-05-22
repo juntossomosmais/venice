71222d58fe8c220f5e4556f8305d321c478a65da
felipefialho <hi@felipefialho.com>
2020-05-22 17:11:49 -0300

    feat(button): add new param to interface

--- packages/core/models/button.model.ts
+++ packages/core/models/button.model.ts
@@ -2,11 +2,13 @@ export interface IButton {
   /**  'default' | 'primary' | 'secondary' */
   color?: 'default' | 'primary' | 'secondary'
   /** boolean */
-  isLoading?: boolean
+  shape?: 'round-square' | 'circle'
   /** boolean */
   disabled?: boolean
   /** href <a> */
   href?: string
+  /** boolean */
+  isLoading?: boolean
   /** 'medium' | 'small' */
   size?: 'medium' | 'small'
   /** 'filled' | 'outlined' | 'text' */
