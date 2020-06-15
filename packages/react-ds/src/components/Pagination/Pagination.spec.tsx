5e9573eb0a209f72d3271ea8ec9a128282f7c995
Henrique Augusto <hick_97@hotmail.com>
2020-06-15 11:31:54 -0300

    chore: formatting spaces

--- packages/react-ds/src/components/Pagination/Pagination.spec.tsx
+++ packages/react-ds/src/components/Pagination/Pagination.spec.tsx
@@ -11,30 +11,38 @@ describe('<Pagination />', () => {
     onChange: jest.fn(),
     isLoading: false,
   }
+
   it('should not display the previous button', () => {
     const { queryByTestId } = render(<Pagination page={1} {...props} />)
+
     expect(queryByTestId('previous-button')).not.toBeInTheDocument()
   })
+
   it('should not display the next button', () => {
     const { queryByTestId } = render(
       <Pagination page={props.count - 1} {...props} />
     )
+
     expect(queryByTestId('next-button')).not.toBeInTheDocument()
   })
+
   it('should increase the range size on the second page', () => {
     const { queryAllByTestId, rerender } = render(
       <Pagination page={1} {...props} />
     )
     const firstRangeSize = queryAllByTestId('page-index').length
     rerender(<Pagination page={2} {...props} />)
+
     expect(queryAllByTestId('page-index')).toHaveLength(firstRangeSize + 1)
   })
+
   it('should increase the range size on the penultimate page', () => {
     const { queryAllByTestId, rerender } = render(
       <Pagination page={props.count} {...props} />
     )
     const firstRangeSize = queryAllByTestId('page-index').length
     rerender(<Pagination page={props.count - 1} {...props} />)
+
     expect(queryAllByTestId('page-index')).toHaveLength(firstRangeSize + 1)
   })
 
@@ -47,6 +55,7 @@ describe('<Pagination />', () => {
 
     expect(queryAllByTestId('page-index')).toHaveLength(firstRangeSize - 1)
   })
+
   it('should decrease the range size on the last page', () => {
     const { queryAllByTestId, rerender } = render(
       <Pagination page={props.count - 1} {...props} />
@@ -66,29 +75,36 @@ describe('<Pagination />', () => {
         isLoading={true}
       />
     )
+
     expect(queryByTestId('previous-button')).toBeDisabled()
     expect(queryByTestId('next-button')).toBeDisabled()
   })
 
   it('should enabled buttons when page is loaded', () => {
     const { queryByTestId } = render(<Pagination page={50} {...props} />)
+
     expect(queryByTestId('previous-button')).not.toBeDisabled()
     expect(queryByTestId('next-button')).not.toBeDisabled()
   })
+
   it('should not render component when page is less than 1', () => {
     const { queryByTestId } = render(<Pagination page={0} {...props} />)
+
     expect(queryByTestId('page-index')).not.toBeInTheDocument()
     expect(queryByTestId('previous-button')).not.toBeInTheDocument()
     expect(queryByTestId('next-button')).not.toBeInTheDocument()
   })
+
   it('should not render component when page is greater than count', () => {
     const { queryByTestId } = render(
       <Pagination page={props.count + 2} {...props} />
     )
+
     expect(queryByTestId('page-index')).not.toBeInTheDocument()
     expect(queryByTestId('previous-button')).not.toBeInTheDocument()
     expect(queryByTestId('next-button')).not.toBeInTheDocument()
   })
+
   it('should not render component when count is less than 1', () => {
     const { queryByTestId } = render(
       <Pagination
@@ -98,6 +114,7 @@ describe('<Pagination />', () => {
         isLoading={props.isLoading}
       />
     )
+
     expect(queryByTestId('page-index')).not.toBeInTheDocument()
     expect(queryByTestId('previous-button')).not.toBeInTheDocument()
     expect(queryByTestId('next-button')).not.toBeInTheDocument()
