2161ec50192a05bc48e70ec9a67f5a875c1371ac
elizeumatheus <elizeumatheus16@gmail.com>
2020-06-16 19:49:44 -0300

    feat: created model for Split Button

---
+++ packages/core/models/splitbutton.model.ts
@@ -0,0 +1,8 @@
+export interface ISplitButton {
+  /** 'large' | 'medium'  */
+  size?: 'large' | 'medium'
+  /**  'default' | 'primary' | 'secondary' */
+  color?: 'default' | 'primary' | 'secondary'
+  /** string */
+  text: string
+}
