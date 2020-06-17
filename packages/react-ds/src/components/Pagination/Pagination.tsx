dc9a4d5f9baa8b2309dbee9cfe5f8356263b8367
Henrique Augusto <hick_97@hotmail.com>
2020-06-17 19:35:25 -0300

    fix: removing data-testid on pagination component

--- packages/react-ds/src/components/Pagination/Pagination.tsx
+++ packages/react-ds/src/components/Pagination/Pagination.tsx
@@ -77,7 +77,6 @@ const Pagination: React.FC<IPagination> = ({
           <button
             className={styles.paginationButton}
             disabled={isLoading}
-            data-testid="previous-button"
             onClick={prevPage}
           >
             <AngleLeft width="12" height="23" viewBox="-1 3 12 7" />
@@ -96,7 +95,6 @@ const Pagination: React.FC<IPagination> = ({
           <button
             className={styles.paginationButton}
             disabled={isLoading}
-            data-testid="next-button"
             onClick={nextPage}
           >
             <AngleRight width="12" height="23" viewBox="-3 3 12 7" />
