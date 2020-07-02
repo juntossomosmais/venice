58b3b7e69b80488a6329a82cf847ddeebcbc98cf
elizeumatheus <elizeumatheus16@gmail.com>
2020-07-02 11:37:56 -0300

    feat: added knob for splitbutton size

--- packages/core/storybook/knobs/split-button.ts
+++ packages/core/storybook/knobs/split-button.ts
@@ -2,7 +2,11 @@ import { boolean, select } from '@storybook/addon-knobs'
 
 const openTypes = ['hover', 'click']
 
+const sizes = ['large', 'medium']
+
 export const knobSplitButtonOpenType = () =>
   select('Open Types', openTypes, openTypes[0])
 
-export const knotSplitButtonIsFitMenu = () => boolean('isFitMenu', false)
+export const knobSplitButtonIsFitMenu = () => boolean('isFitMenu', false)
+
+export const knobSplitButtonSizes = () => select('Sizes', sizes, sizes[0])
