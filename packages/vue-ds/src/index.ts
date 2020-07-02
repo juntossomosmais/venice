725a46ce9dbc6368d51aa6d5b2c145c70b8947ba
Henrique Augusto <hick_97@hotmail.com>
2020-07-02 18:12:59 -0300

    chore: exporting pagination

--- packages/vue-ds/src/index.ts
+++ packages/vue-ds/src/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from './components/Button/Button.vue'
+export { default as Pagination } from './components/Pagination/Pagination.vue'
 export { default as Spinner } from './components/Spinner/Spinner.vue'
 export { default as SplitButton } from './components/SplitButton/SplitButton.vue'
 export { default as Stepper } from './components/Stepper/Stepper.vue'
