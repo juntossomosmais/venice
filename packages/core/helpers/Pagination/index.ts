77a8d022f2dfa68f19559a364d1d33cf960d49ef
Henrique Augusto <hick_97@hotmail.com>
2020-07-08 14:01:59 -0300

    chore: updating helpers content

--- packages/core/helpers/Pagination/index.ts
+++ packages/core/helpers/Pagination/index.ts
@@ -1,15 +1,19 @@
-export const hasNextRange = (offset, currentPage, count) =>
+export const hasNextRange = (offset = 0, currentPage = 0, count = 0) =>
   currentPage + offset <= count
 
-export const hasPreviousRange = (offset, currentPage) =>
+export const hasPreviousRange = (offset = 0, currentPage = 0) =>
   currentPage - offset > 0
 
-const getRangeOffsets = (currentPage, isMobile) =>
+const getRangeOffsets = (currentPage = 0, isMobile = false) =>
   isMobile
     ? [currentPage - 2, currentPage + 1]
     : [currentPage - 3, currentPage + 2]
 
-export const getRangeIndexes = (count, currentPage, isMobile) => {
+export const getRangeIndexes = (
+  count = 0,
+  currentPage = 0,
+  isMobile = false
+) => {
   const [startOfRange, endOfRange] = getRangeOffsets(currentPage, isMobile)
   if (startOfRange < 0) {
     return [0, endOfRange]
