a4df10e6f5b8a25837545091039a895457a045a9
Pedro Henrique <ph.barbosilva@gmail.com>
2020-07-15 16:46:54 -0300

    refactor: rollup config

--- packages/react-ds/rollup.config.js
+++ packages/react-ds/rollup.config.js
@@ -1,50 +1,90 @@
+import babel from '@rollup/plugin-babel'
 import autoprefixer from 'autoprefixer'
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
-import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
+import sourceMaps from 'rollup-plugin-sourcemaps'
 import typescript from 'rollup-plugin-typescript2'
 
-import pkg from './package.json'
+const pkg = require('./package.json')
+
+const input = 'src/index.ts'
+const extensions = ['.ts', '.tsx']
+
+const plugins = [
+  typescript({
+    tsconfigDefaults: {
+      exclude: [
+        // all TS test files, regardless whether co-located or in test/ etc
+        '**/*.spec.ts',
+        '**/*.test.ts',
+        '**/*.spec.tsx',
+        '**/*.test.tsx',
+      ],
+      compilerOptions: {
+        sourceMap: true,
+        declaration: true,
+        jsx: 'react',
+      },
+      tsconfigOverride: {
+        compilerOptions: {
+          // TS -> esnext, then leave the rest to babel-preset-env
+          target: 'esnext',
+        },
+      },
+    },
+    typescript: require('typescript'),
+  }),
+  postcss({
+    extract: false,
+    modules: true,
+    use: ['sass'],
+    plugins: [autoprefixer()],
+  }),
+  resolve({
+    mainFields: ['module', 'main', 'browser'],
+    extensions: ['.js', '.jsx'],
+  }),
+  commonjs(),
+  sourceMaps(),
+  babel({
+    extensions,
+    babelHelpers: 'runtime',
+    include: 'src/**/*',
+    exclude: 'node_modules/**',
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: false,
+          loose: true,
+        },
+      ],
+    ],
+    plugins: ['@babel/plugin-transform-runtime'],
+  }),
+]
+
+const outputOptions = {
+  sourcemap: true,
+  freeze: false,
+  esModule: true,
+}
 
 export default {
-  input: 'src/index.tsx',
+  input,
   output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true,
-    },
     {
       file: pkg.module,
       format: 'es',
-      sourcemap: true,
+      ...outputOptions,
+    },
+    {
+      file: pkg.main,
+      format: 'cjs',
+      ...outputOptions,
     },
   ],
-  plugins: [
-    external(),
-    resolve({
-      browser: true,
-    }),
-    typescript(),
-    commonjs({
-      include: /node_modules/,
-      exclude: ['**/*.stories.js'],
-      namedExports: {
-        'node_modules/react/react.js': [
-          'Children',
-          'Component',
-          'PropTypes',
-          'createElement',
-        ],
-        'node_modules/react-dom/index.js': ['render'],
-      },
-    }),
-    postcss({
-      extract: false,
-      modules: true,
-      use: ['sass'],
-      plugins: [autoprefixer()],
-    }),
-  ],
+  external: Object.keys(pkg.peerDependencies),
+  plugins,
 }
