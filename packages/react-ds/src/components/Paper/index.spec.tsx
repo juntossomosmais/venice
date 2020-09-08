6cf29ef782d798b846571518832300d659e76843
felipefialho <hi@felipefialho.com>
2020-09-08 18:46:11 -0300

    feat(paper): add test

--- packages/react-ds/src/components/Paper/index.spec.tsx
+++ packages/react-ds/src/components/Paper/index.spec.tsx
@@ -5,16 +5,9 @@ import { render } from '@testing-library/react'
 
 import { Paper } from '.'
 
-const Mount = (props) => <Paper {...props}>Message</Paper>
-
 describe('<Paper />', () => {
-  it('Should display Paper base', () => {
-    const { container, queryByText } = render(<Mount />)
-    const icon = container.querySelector('svg')
-
-    expect(container.firstChild).toHaveClass('with-icon')
-    expect(icon).toBeInTheDocument()
-
-    expect(queryByText('Message')).toBeInTheDocument()
+  it('should have "paper" classname when render', () => {
+    const { container } = render(<Paper>Default</Paper>)
+    expect(container.firstChild).toHaveClass('paper')
   })
 })
