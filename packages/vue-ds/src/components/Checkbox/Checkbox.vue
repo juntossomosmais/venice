a925c3b8dfa1f0b1905352c6713995e49df274eb
Tássio <tassio72@hotmail.com>
2020-09-10 17:14:39 -0300

    feat(vue): checkbox component

---
+++ packages/vue-ds/src/components/Checkbox/Checkbox.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div>
+      <input
+        type="checkbox"
+        :id="selfId"
+        @change="$emit('input', $event.target.checked)"
+        v-bind="{ ...props }"
+      />
+      <label v-if="label" :for="selfId" class="label">
+        {{ label }}
+      </label>
+    </div>
+    <div v-if="error" class="error">
+      {{ error }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { IField } from '@juntossomosmais/venice-types'
+import uniqid from 'uniqid'
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component({})
+export default class Checkbox extends Vue {
+  @Prop({ default: '' }) id!: string
+  @Prop({ default: '' }) label!: IField['label']
+  @Prop({ default: '' }) error!: IField['error']
+  @Prop({ default: null }) props
+
+  private selfId = this.id || uniqid(`input__`)
+}
+</script>
