fe8ad3a4b5313b18d0d81b08bac56ed680d377f6
Pedro Henrique <ph.barbosilva@gmail.com>
2020-09-10 13:41:38 -0300

    refactor: generate fontello assets on postinstall script

--- scripts/moveFontelloAssets.js
+++ scripts/moveFontelloAssets.js
@@ -7,7 +7,12 @@ async function moveFontelloToStorybookPublicFolder(error, files) {
   try {
     console.log('Moving fontello assets to Storybook public folder')
 
-    await fs.move(fontelloDirectory, 'public', { overwrite: true })
+    await fs.copy(fontelloDirectory, 'packages/react-ds/public')
+    await fs.copy(fontelloDirectory, 'packages/vue-ds/public')
+
+    console.log('Deleting fontello original folder')
+
+    await fs.remove(fontelloDirectory)
 
     console.log('Success!')
   } catch (err) {
