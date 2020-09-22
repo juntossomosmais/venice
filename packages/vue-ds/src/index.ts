5f8db3b0826de82e5e6d7ad9dc64c3d957d8a858
felipefialho <hi@felipefialho.com>
2020-09-22 16:51:31 -0300

    feat(breadcrumb): export lost file

--- packages/vue-ds/src/index.ts
+++ packages/vue-ds/src/index.ts
@@ -1,4 +1,5 @@
 export { default as Breadcrumb } from './components/Breadcrumb/Breadcrumb.vue'
+export { default as BreadcrumbItem } from './components/Breadcrumb/BreadcrumbItem.vue'
 export { default as Button } from './components/Button/Button.vue'
 export { default as Pagination } from './components/Pagination/Pagination.vue'
 export { default as Paper } from './components/Paper/Paper.vue'
