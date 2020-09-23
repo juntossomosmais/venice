869416c1c67bf9a52f40780717708a4de7acf66f
Tássio <tassio72@hotmail.com>
2020-09-23 17:25:29 -0300

    feat: missing import

--- packages/vue-ds/src/index.ts
+++ packages/vue-ds/src/index.ts
@@ -1,6 +1,7 @@
 export { default as Breadcrumb } from './components/Breadcrumb/Breadcrumb.vue'
 export { default as BreadcrumbItem } from './components/Breadcrumb/BreadcrumbItem.vue'
 export { default as Button } from './components/Button/Button.vue'
+export { default as Checkbox } from './components/Checkbox/Checkbox.vue'
 export { default as Pagination } from './components/Pagination/Pagination.vue'
 export { default as Paper } from './components/Paper/Paper.vue'
 export { default as Spinner } from './components/Spinner/Spinner.vue'
