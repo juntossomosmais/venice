fccf2b6072367a254582c45dedac3c4175a5e8e0
felipefialho <hi@felipefialho.com>
2020-12-22 17:08:59 -0300

    chore(sonar): add config to export coverage

--- packages/react-ds/jest.config.js
+++ packages/react-ds/jest.config.js
@@ -4,4 +4,9 @@ module.exports = {
   ...base,
   displayName: 'React DS',
   transformIgnorePatterns: ['node_modules/(?!.*?/es/.*\\.js)'],
+  collectCoverageFrom: [
+    'src/**/*.{js, jsx, ts, tsx}',
+    '!src/index.js',
+    '!src/react-app-env.d.ts',
+  ],
 }
--- packages/vue-ds/jest.config.js
+++ packages/vue-ds/jest.config.js
@@ -4,4 +4,12 @@ module.exports = {
   ...base,
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   testMatch: ['<rootDir>/src/**/*.(spec|test).(tsx|jsx)'],
+  collectCoverageFrom: [
+    'src/**/*.vue',
+    '!**/node_modules/**',
+    '!src/index.js',
+    '!src/shims-tsx.d.js',
+    '!src/shims-vue.js',
+    '!src/**/icons/*.vue',
+  ],
 }
