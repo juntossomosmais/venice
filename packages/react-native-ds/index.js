db12079615c18f401046d1f3201678cd14f5a154
Igor Soares <igor.soares@juntossomosmais.com.br>
2022-08-11 19:15:02 -0300

    feat(rn-ds): add object with new varibles

--- packages/react-native-ds/index.js
+++ packages/react-native-ds/index.js
@@ -1,9 +1,1 @@
-import cssToJS from 'transform-css-to-js'
-
-import css from './dist/styles.css'
-console.log('🚀 ~ file: test.js ~ line 4 ~ css', css)
-
-// const reactNativeCompatibleCSS = cssToJS(result.css.toString('utf-8'), true)
-const reactNativeCompatibleCSS = cssToJS(css, true)
-
-export const styles = reactNativeCompatibleCSS
+export * from './styles/variables'
