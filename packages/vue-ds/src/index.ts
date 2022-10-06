add33dac6af7d49ebe3718eb215a31e84e917855
Alan Schio <alanbschio@gmail.com>
2022-10-06 14:29:08 -0300

    feat(LogoAnimation): add logo animation for jsm

--- packages/vue-ds/src/index.ts
+++ packages/vue-ds/src/index.ts
@@ -3,6 +3,7 @@ export { default as BreadcrumbItem } from './components/Breadcrumb/BreadcrumbIte
 export { default as Button } from './components/Button/Button.vue'
 export { default as Checkbox } from './components/Checkbox/Checkbox.vue'
 export { default as LoadingLogo } from './components/LoadingLogo/LoadingLogo.vue'
+export { default as LogoAnimation } from './components/LogoAnimation/LogoAnimation.vue'
 export { default as Pagination } from './components/Pagination/Pagination.vue'
 export { default as Paper } from './components/Paper/Paper.vue'
 export { default as Tag } from './components/Tag/Tag.vue'
