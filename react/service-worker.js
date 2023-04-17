a3058a97ea38723615ec6aa3c9b910d052929617
jowjow22 <51102351+jowjow22@users.noreply.github.com>
2023-04-17 17:30:46 +0000

    Deploying to gh-pages from @ juntossomosmais/venice@cf696de6a61be4cd5ef5e3fb692db564ea12fe34 🚀

--- react/service-worker.js
+++ react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.d363b6d06820897fb3b511c24a89acbc.js"
+  "precache-manifest.0e04fadc2fcecc1d1cb17ce40cf98591.js"
 );
 
 self.addEventListener('message', (event) => {
